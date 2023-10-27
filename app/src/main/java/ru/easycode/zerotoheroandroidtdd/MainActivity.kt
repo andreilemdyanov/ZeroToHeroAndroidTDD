f5cb9ba9afa3dc1aa0e5eb75da3157edf54e7fea
A_Lemdyanov_56065 <A.Lemdyanov@rendez-vous.ru>
2023-10-27 20:26:05 +0300

    test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,23 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.titleTextView)
+        btn = findViewById(R.id.changeButton)
+
+        textView.freezesText = true
+        btn.setOnClickListener {
+            textView.text = resources.getText(R.string.title_text_alt)
+        }
     }
 }

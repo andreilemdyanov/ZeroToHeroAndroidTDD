9b902ec811311ef6604e81e877b19fd1b5f039dd
A_Lemdyanov_56065 <A.Lemdyanov@rendez-vous.ru>
2023-11-07 21:16:25 +0300

    test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,60 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.LinearLayout.TEXT_ALIGNMENT_CENTER
+import android.widget.LinearLayout.VERTICAL
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rootLayout: LinearLayout
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rootLayout = LinearLayout(this)
+            .apply {
+                id = R.id.rootLayout
+                orientation = VERTICAL
+                gravity = TEXT_ALIGNMENT_CENTER
+            }
+        textView = TextView(this)
+            .apply {
+                id = R.id.titleTextView
+                text = "Hello World!"
+            }
+        val removeBtn = Button(this)
+            .apply {
+                id = R.id.removeButton
+                text = "remove"
+            }
+
+        rootLayout.addView(textView)
+        rootLayout.addView(removeBtn)
+        setContentView(rootLayout)
+
+
+        removeBtn.setOnClickListener {
+            rootLayout.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(STATE_TEXT, !rootLayout.children.contains(textView))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.getBoolean(STATE_TEXT))
+            rootLayout.removeView(textView)
+    }
+
+    companion object {
+        private const val STATE_TEXT = "state_text"
     }
 }

18f6a01727998674a0095c2446a51ee9a30714e6
A_Lemdyanov_56065 <a.lemdyanov@rendez-vous.ru>
2023-12-11 20:46:06 +0300

    test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,32 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
+    private lateinit var actionButton: Button
+    private lateinit var progressBar: ProgressBar
+    private val viewModel = MainViewModel(LiveDataWrapper.Base(), Repository.Base())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        actionButton = findViewById(R.id.actionButton)
+        progressBar = findViewById(R.id.progressBar)
+
+        actionButton.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.observe(this) {
+            it.apply(titleTextView, actionButton, progressBar)
+        }
     }
 }

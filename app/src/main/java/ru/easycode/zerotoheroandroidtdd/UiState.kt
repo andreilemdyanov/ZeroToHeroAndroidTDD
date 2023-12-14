5992d4d279bfb74f8c9c7820c1151942018e92b1
A_Lemdyanov_56065 <a.lemdyanov@rendez-vous.ru>
2023-12-14 12:54:01 +0300

    test passed

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,37 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
+import java.io.Serializable
+
+interface UiState: Serializable {
+
+    fun apply(titleTextView: TextView, actionButton: Button, progressBar: ProgressBar)
+
+    object ShowProgress : UiState {
+        override fun apply(
+            titleTextView: TextView,
+            actionButton: Button,
+            progressBar: ProgressBar
+        ) {
+            titleTextView.isVisible = false
+            actionButton.isEnabled = false
+            progressBar.isVisible = true
+        }
+    }
+
+    data class ShowData(private val text: String) : UiState {
+        override fun apply(
+            titleTextView: TextView,
+            actionButton: Button,
+            progressBar: ProgressBar
+        ) {
+            titleTextView.text = text
+            titleTextView.isVisible = true
+            actionButton.isEnabled = true
+            progressBar.isVisible = false
+        }
+    }
+}
